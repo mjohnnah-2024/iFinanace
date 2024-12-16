7169eca85d60ddd3213bf08c335701acbf630316
Mark Johnnah <markjohnnah@gmail.com>
2024-12-16 12:39:20 +1000

    update contact info

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -23,7 +23,7 @@
                 <p class="mail-text text-black">Welcome to iFinance Website</p>
             </div>
             <div class="col-xl-8 col-lg-7 col-md-12 col-sm-12 col-12 text-right">
-                <div class="top-nav"> <span class="top-text"><a href="/" class="text-black">View Locations</a> </span> <span class="top-text"><a href="/" class="text-black">+1800-123-4567</a></span> <span class="top-text"><a href="/loan-eligibility" class="text-black">EMI calculator</a></span> </div>
+                <div class="top-nav"> <span class="top-text"><a href="/" class="text-black">View Locations</a> </span> <span class="top-text"><a href="/" class="text-black">+675-311-2200</a></span> <span class="top-text"><a href="/loan-eligibility" class="text-black">EMI calculator</a></span> </div>
             </div>
         </div>
     </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -248,22 +248,22 @@
                         <div class="mb40"><i class="icon-calendar-3 icon-2x icon-default"></i></div>
                         <h2 class="capital-title">Apply For Loan</h2>
                         <p>Looking to buy a car or home loan? then apply for loan now.</p>
-                        <a href="#" class="btn-link">Get Appointment</a> </div>
+                        <a href="/contact-us" class="btn-link">Get Appointment</a> </div>
                 </div>
                  <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
                     <div class="bg-white bg-boxshadow pinside40 outline text-center mb30">
                         <div class="mb40"><i class="icon-phone-call icon-2x icon-default"></i></div>
                         <h2 class="capital-title">Call us at </h2>
-                        <h1 class="text-big">800-123-456 / 789 </h1>
+                        <h1 class="text-big">3112200 /3112202 </h1>
                         <p>lnfo@loanadvisor.com</p>
-                        <a href="#" class="btn-link">Contact us</a> </div>
+                        <a href="/contact-us" class="btn-link">Contact us</a> </div>
                 </div>
                 <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
                     <div class="bg-white bg-boxshadow pinside40 outline text-center mb30">
                         <div class="mb40"> <i class="icon-users icon-2x icon-default"></i></div>
                         <h2 class="capital-title">Talk to Advisor</h2>
                         <p>Need to loan advise? Talk to our Loan advisors.</p>
-                        <a href="#" class="btn-link">Meet The Advisor</a> </div>
+                        <a href="/contact-us" class="btn-link">Meet The Advisor</a> </div>
                 </div>
             </div>
         </div>
