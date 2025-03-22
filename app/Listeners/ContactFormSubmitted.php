090f65b0b4e7e1effc8ccf722ff1bd4dfa3725eb
Mark Johnnah <markjohnnah@gmail.com>
2025-03-22 23:23:18 +1000

    udated receiver email

--- app/Listeners/ContactFormSubmitted.php
+++ app/Listeners/ContactFormSubmitted.php
@@ -26,7 +26,7 @@ class ContactFormSubmitted
     public function handle(SubmitContactForm $event): void
     {
         //Mail::to('admin@ifinancepng.com')->send(new NotifyAdmin($event->data));
-        Notification::route('mail', 'ifinancepng@nextgenpng.net')->notify(new NotifyAdmin($event->data));
+        Notification::route('mail', 'markjohnnah@gmail.com')->notify(new NotifyAdmin($event->data));
         //Notification::route('mail', $event->data['email'])->notify(new NotifyClient($event->data));
     }
 }
